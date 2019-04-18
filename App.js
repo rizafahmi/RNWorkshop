29f0092ed897e9e2f2e45106143735711032f5b6
Riza Fahmi <rizafahmi@gmail.com>
2019-04-18 13:38:45 +0700

    Finish layout

--- App.js
+++ App.js
@@ -6,9 +6,17 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.instructions}>Hello, React Native!</Text>
-        <Image style={styles.image} source={require('./img/hacktiv8.png')} />
-        <Button title="Learn More" color="mediumaquamarine" />
+        <View style={[styles.box, styles.headerContainer]}>
+          <Text style={styles.headerText}>HEADER</Text>
+        </View>
+        <View style={[styles.box, styles.contentContainer]}>
+          <Text style={styles.content}>Content here...</Text>
+        </View>
+        <View style={[styles.box, styles.footerContainer]}>
+          <Text style={styles.footer}>
+            &copy; 2019 RN Workshop. All right reserved.
+          </Text>
+        </View>
       </View>
     );
   }
@@ -17,17 +25,33 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column'
+  },
+  box: {
+    flex: 1
+  },
+  headerContainer: {
+    backgroundColor: '#343A40',
+    flex: 1,
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 32,
+    color: '#fff'
+  },
+  contentContainer: {
+    backgroundColor: '#F7F8F9',
+    flex: 10,
+    marginTop: 22,
+    marginLeft: 22
+  },
+  footerContainer: {
+    backgroundColor: '#6D757D',
+    flex: 0.5,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  },
-  image: {
-    width: '50%',
-    height: '25%'
+  footer: {
+    color: '#F7F8F9'
   }
 });
