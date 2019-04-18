8539970f5051c7b5b189c14135c243e728b26236
Riza Fahmi <rizafahmi@gmail.com>
2019-04-18 13:11:34 +0700

    Finish basic UIs

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native Workshop!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>Hello, React Native!</Text>
+        <Image style={styles.image} source={require('./img/hacktiv8.png')} />
+        <Button title="Learn More" color="mediumaquamarine" />
       </View>
     );
   }
@@ -36,14 +21,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5
+  },
+  image: {
+    width: '50%',
+    height: '25%'
   }
 });
