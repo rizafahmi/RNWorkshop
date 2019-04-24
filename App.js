a2e33bd439f96485a747a7c47f9663d2045c15df
Riza Fahmi <rizafahmi@gmail.com>
2019-04-24 08:57:34 +0700

    Finish flatlist

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Image, Text, View } from 'react-native';
+import { Button, Image, Text, View, FlatList } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import Detail from './screens/Detail.js';
@@ -7,36 +7,6 @@ import { database } from './src/firebase.js';
 import styles from './styles.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      podcasts: []
-    };
-  }
-  componentDidMount() {
-    database.ref('/podcasts').on('value', (snapshot) => {
-      this.setState({
-        podcasts: snapshot.val()
-      });
-    });
-  }
-  renderItem = (podcast, index) => (
-    <View key={index} style={styles.itemContainer}>
-      <View style={styles.itemImageContainer}>
-        <Image style={styles.itemImage} source={{ uri: podcast.image }} />
-      </View>
-      <View style={styles.itemTextContainer}>
-        <Text style={styles.itemText}>{podcast.title}</Text>
-        <Text style={styles.itemUrl}>{podcast.url}</Text>
-        <Button
-          title="View Detail"
-          onPress={() =>
-            this.props.navigation.navigate('Detail', { podcast: podcast })
-          }
-        />
-      </View>
-    </View>
-  );
   static navigationOptions = {
     title: '🚀 PodSpace',
     headerStyle: {
@@ -47,11 +17,53 @@ class App extends Component {
       color: '#fff'
     }
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      podcasts: null
+    };
+  }
+  componentDidMount() {
+    database.ref('/podcasts').on('value', (snapshot) => {
+      this.setState({
+        podcasts: snapshot.val()
+      });
+    });
+  }
+  renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <View style={styles.itemImageContainer}>
+        <Image style={styles.itemImage} source={{ uri: item.image }} />
+      </View>
+      <View style={styles.itemTextContainer}>
+        <Text style={styles.itemText}>{item.title}</Text>
+        <Text style={styles.itemUrl}>{item.url}</Text>
+        <Button
+          title="View Detail"
+          onPress={() =>
+            this.props.navigation.navigate('Detail', { podcast: item })
+          }
+        />
+      </View>
+    </View>
+  );
   render() {
-    const podcastList = this.state.podcasts.map(this.renderItem);
+    const podcasts =
+      this.state.podcasts !== null &&
+      this.state.podcasts.filter((podcast) => podcast !== null);
     return (
       <View style={styles.container}>
-        <View style={[styles.box, styles.contentContainer]}>{podcastList}</View>
+        <View style={[styles.box, styles.contentContainer]}>
+          {podcasts.length > 0 ? (
+            <FlatList
+              data={podcasts}
+              keyExtractor={(item) => item.id}
+              renderItem={this.renderItem}
+            />
+          ) : (
+            <Text>Loading...</Text>
+          )}
+        </View>
         <View style={[styles.box, styles.footerContainer]}>
           <Text style={styles.footer}>
             &copy; 2019 RN Workshop. All right reserved.
